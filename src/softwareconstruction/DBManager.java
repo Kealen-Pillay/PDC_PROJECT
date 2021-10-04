b066d35cc0080b3bc5981cc5286740dd64572655
Kealen-Pillay <kealenpillay@gmail.com>
2021-10-04 16:42:12 +1300

    Embedded Database Implementation

---
+++ src/softwareconstruction/DBManager.java
@@ -0,0 +1,23 @@
+
+package softwareconstruction;
+
+/**
+ *
+ * @author kealenpillay
+ */
+public class DBManager 
+{
+    //----------------------------------------------------- Instance Variables ---------------------------------------------------------
+    private static final String USERNAME = "pdc";
+    private static final String PASSWORD = "pdc";
+    private static final String URL = "jdbc:derby:VirtualPetGameDB; create=true";
+    
+    //----------------------------------------------------- Constructor ---------------------------------------------------------
+    public DBManager()
+    {
+        
+    }
+    
+    //----------------------------------------------------- Methods ---------------------------------------------------------
+    
+}
