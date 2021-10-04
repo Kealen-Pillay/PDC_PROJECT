4dc017f477357af77e56b5e4c6cb10c8b98b6ced
Kealen-Pillay <kealenpillay@gmail.com>
2021-10-04 17:03:57 +1300

    Added methods to DBManager class

--- src/softwareconstruction/DBManager.java
+++ src/softwareconstruction/DBManager.java
@@ -1,6 +1,10 @@
 
 package softwareconstruction;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author kealenpillay
@@ -11,13 +15,56 @@ public class DBManager
     private static final String USERNAME = "pdc";
     private static final String PASSWORD = "pdc";
     private static final String URL = "jdbc:derby:VirtualPetGameDB; create=true";
+    private Connection conn;
     
     //----------------------------------------------------- Constructor ---------------------------------------------------------
     public DBManager()
     {
-        
+        this.establishConnection();
     }
     
     //----------------------------------------------------- Methods ---------------------------------------------------------
+
+    /**
+     * @return the conn
+     */
+    public Connection getConn() 
+    {
+        return conn;
+    }
+    
+    /**
+     * Establish a connection to the database
+     */
+    public void establishConnection() 
+    {
+        if(this.conn == null) 
+        {
+            try 
+            {
+                conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                System.out.println(URL + " Get Connected Successfully ....");
+            } 
+            catch (SQLException ex) 
+            {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+    public void closeConnections() 
+    {
+        if(conn != null) 
+        {
+            try 
+            {
+                conn.close();
+            } 
+            catch (SQLException ex) 
+            {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
     
 }
