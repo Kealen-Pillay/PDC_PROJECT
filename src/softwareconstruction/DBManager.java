acc0800e23283ae6623349b5b365c4425eb9b4ac
Kealen-Pillay <kealenpillay@gmail.com>
2021-10-08 20:14:22 +1300

    Removed Unneccesary Imports

--- src/softwareconstruction/DBManager.java
+++ src/softwareconstruction/DBManager.java
@@ -4,9 +4,6 @@ package softwareconstruction;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
