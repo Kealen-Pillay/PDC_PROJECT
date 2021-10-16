35a2fd5258374352ca451e5fe5f9e78232990f3d
Kealen-Pillay <kealenpillay@gmail.com>
2021-10-16 19:45:02 +1300

    removed unused imports

--- src/softwareconstruction/Model.java
+++ src/softwareconstruction/Model.java
@@ -1,11 +1,8 @@
 package softwareconstruction;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Observable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
